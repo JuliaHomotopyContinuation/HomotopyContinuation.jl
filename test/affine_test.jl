b32747bbfb63dc9540b87f51fb91d29f1d7b06fd
Sascha Timme <sascha.timme@gmail.com>
2017-08-03 16:41:12 +0200

    Add affine as PredictorCorrect algorithm

---
+++ test/affine_test.jl
@@ -0,0 +1,24 @@
+using HomotopyContinuation
+
+@testset "affine" begin
+
+    @testset "simplest system" begin
+        x = MPoly.generator(Complex128, :x)
+        f = (x - 2.0) * (x - (2.5+ 4.0im))
+        g = (x - 4.3im) * (x + (2.1 - 4im))
+
+        F = MPoly.system([f])
+        G = MPoly.system([g])
+
+        H = StraightLineHomotopy(G, F)
+        res = solve(H, [[-2.1 + 4.0im], [4.3im]], PredictorCorrector.Affine(), report_progress=false)
+
+        @test res[1].retcode == :Success
+        @test res[2].retcode == :Success
+
+        @test real(res[1].solution) ≈ [2.0]
+        @test real(res[2].solution) ≈ [2.5]
+        @test imag(res[2].solution) ≈ [4.0]
+    end
+
+end
