67eb7eaf7fe16d29875847246cd731baa48e40a5
Sascha Timme <sascha.timme@gmail.com>
2017-08-08 16:25:02 +0200

    Add GammaTrickHomotopy (#4)
    * Add GammaTrickHomotopy

---
+++ src/Homotopy/gamma_trick.jl
@@ -0,0 +1,105 @@
+"""
+    GammaTrickHomotopy(G, F[, γ])
+
+This yields the homotopy (1-t)F + tγ(t)G, with t from 1 to 0.
+The gamma trick is a special type of homotopy interpolation.
+One chooses an ``γ`` uniformly from the (complex) unit circle.
+This yields a more generic interpolation.
+
+Possible to create it optionally with a seed.
+"""
+struct GammaTrickHomotopy{T<:Complex} <: AbstractHomotopy{T}
+    start::Vector{Poly{T}}
+    target::Vector{Poly{T}}
+    γ::T
+
+    function GammaTrickHomotopy{T}(start::Vector{Poly{T}}, target::Vector{Poly{T}}, γ::T) where {T<:Complex}
+        N_start = nvars(start)
+        N_target = nvars(target)
+        n_start = nequations(start)
+        n_target = nequations(target)
+
+        if (N_start != N_target)
+            return error("The number of unknowns of start and target system doesn't match." *
+                "The start system of the homotopy has $N_start unknowns and the target system has $N_target unknowns.")
+        end
+
+        if (n_start != n_target)
+            return error("The number of equations of start and target system doesn't match." *
+                "The start system of the homotopy has $n_start equations and the target system has $n_target equations.")
+        end
+
+        new(start, target, γ)
+    end
+end
+
+GammaTrickHomotopy(start::Vector{Poly{T}}, target::Vector{Poly{T}}, γ::T) where {T<:Complex} = GammaTrickHomotopy{T}(start, target, γ)
+GammaTrickHomotopy(start::Poly{T}, target::Poly{T}, γ::T) where {T<:Complex} = GammaTrickHomotopy([start], [target], γ)
+
+GammaTrickHomotopy(start, target) = GammaTrickHomotopy(start, target, exp(im * (rand() * 2π - π)))
+function GammaTrickHomotopy(start, target, seed::Int)
+    srand(seed)
+    θ = rand() * 2π - π
+    GammaTrickHomotopy(start, target, exp(im * θ))
+end
+
+function evaluate(H::GammaTrickHomotopy{T}, x::Vector{T}, t::Float64) where {T<:Complex}
+    (1 - t) * evaluate(H.target, x) + t * H.γ * evaluate(H.start, x)
+end
+function evaluate(H::GammaTrickHomotopy{T}, x::Vector{S}, t::Float64) where {T<:Complex, S<:Number}
+    u = convert(Vector{T}, x)
+    (1 - t) * evaluate(H.target, u) + t * H.γ * evaluate(H.start, u)
+end
+
+function jacobian(H::GammaTrickHomotopy{T}) where {T<:Number}
+    J_s = jacobian(H.start)
+    J_t = jacobian(H.target)
+
+    return (x::Vector{T}, t::Float64) -> t * H.γ * J_s(x) + (1 - t) * J_t(x)
+end
+dt(H::GammaTrickHomotopy{T}) where {T<:Number} = (x::Vector{T}, ::Float64) -> evaluate(H.start, x) - evaluate(H.target, x)
+
+function homogenize(H::GammaTrickHomotopy)
+    N = nvars(H)
+    n = nequations(H)
+    # currently square system
+    if N == n
+        return GammaTrickHomotopy(homogenize(H.start), homogenize(H.target), H.γ)
+    elseif N == n + 1
+        if is_homogenous(H.start) && is_homogenous(H.start)
+           return H
+        else
+            return error("The Homotopy has $(N) unknowns and $(n) equations. Expected that the polynomials are already homogenous which is not the case.")
+        end
+    else
+        return error("The Homotopy has $(N) unknowns and $(n) equations. Excepted $(n) equations and $(n) or $(n+1) unknowns.")
+    end  
+end
+nvars(H::GammaTrickHomotopy) = nvars(H.start)
+nequations(H::GammaTrickHomotopy) = nequations(H.start)
+degrees(H::GammaTrickHomotopy) = max.(degrees(H.start), degrees(H.target))
+startsystem(H::GammaTrickHomotopy) = H.start
+targetsystem(H::GammaTrickHomotopy) = H.target
+
+"""
+    weyl_norm(H, t)
+
+Computes the weyl_norm of the homotopy to the given time `t`.
+
+## Explanation
+For ``H = (1-t)F+tγG`` we have
+```math
+\begin{align*}
+<H,H> &= <(1-t)F+tγG,(1-t)F+tγG> \\
+      &= <(1-t)F,(1-t)F+tγG> + <tγG,(1-t)F+tγG> \\
+      &= <(1-t)F,(1-t)F> + <(1-t)F,tγG> + <tγG,(1-t)F> + <tγG,tγG> \\
+      &= <(1-t)F,(1-t)F> + 2*real(<(1-t)F,tγG>) + <tγG,tγG> \\
+      &= (1-t)^2<F,F> + (1-t)t*2*real(γ)*real(<F,G>) + abs(γ)*t^2<G,G> \\
+\end{align*}
+```
+"""
+function weyl_norm(H::GammaTrickHomotopy{T}, t::Float64) where {T<:Complex}
+    F = H.target
+    G = H.start
+    sqrt((1 - t)^2 * weyl_dot(F, F) + (1 - t) * t * 2 * real(H.γ) * real(weyl_dot(F, G)) + t^2 * abs2(H.γ) * weyl_dot(G, G))
+end
--- test/straight_line_test.jl
+++ test/straight_line_test.jl
@@ -10,6 +10,9 @@ using Base.Test
 
         H = StraightLineHomotopy(g, f)
         @test typeof(H)<:StraightLineHomotopy{Float64}
+        a = MPoly.generators(Complex128, :a)
+        @test_throws MethodError StraightLineHomotopy(g, MPoly.system(a))
+        @test_throws ErrorException StraightLineHomotopy(MPoly.system(x^2+3y, y^2-x), MPoly.system(x^2+3y))
 
         @test evaluate(H, [2.0, 1.0], 1.0) == [-1.0]
         @test evaluate(H, [2.0, 1.0], 0.0) == [7.0]
@@ -39,71 +42,5 @@ using Base.Test
         K = homogenize(H)
         
         @test nvars(K) == 3
-
-
-    #     H = StraightLineHomotopy(g, f)
-    #     @test typeof(H)<:StraightLineHomotopy{Float64}
-
-    #     @test evaluate(H, [2.0, 1.0], 1.0) == [-1.0]
-    #     @test evaluate(H, [2.0, 1.0], 0.0) == [7.0]
-
-    #     J_H = jacobian(H)
-
-    #     @test J_H([1.0, 1.0], 0.0) == [2 3]
-    #     @test J_H([1.0, 1.0], 1.0) == [-1 2]
-
-    #     ∂H∂t = dt(H)
-
-    #     ## time derivate is independent of t
-    #     @test ∂H∂t([1.0,1.0], 0.23) == [-4]
-
-    #     @test degrees(H) == [2]
-    #     @test startsystem(H) == g
-    #     @test targetsystem(H) == f
     end
-
-    # @testset "eval" begin
-    #     x, y = MPoly.generators(Float64, :x, :y)
-    #     f = MPoly.system(3x^2*y^2+4x^3*y+2x*y^2)
-    #     @test evaluate(f, [3.0, 5.0]) == [1365.0]
-    # end
-
-    # @testset "jacobian" begin
-    #     x, y = MPoly.generators(Float64, :x, :y)
-    #     f = polysystem(3x^2*y^2+4x^3*y+2x*y^2)
-
-    #     df = jacobian(f)
-    #     @test df([2.0,2.0]) == [152.0 96.0]
-    # end
-
-    # @testset "is_homogenous" begin
-    #     x, y = MPoly.generators(Float64, :x, :y)
-
-    #     @test is_homogenous(MPoly.system(3x^2*y+4x^3+2x*y^2)) == true
-    #     @test is_homogenous(MPoly.system(3x^2*y+4x^3+2x*y)) == false
-    # end
-
-    # @testset "degrees, nequations, nvars" begin
-    #     x, y = MPoly.generators(Float64, :x, :y)
-    #     F = MPoly.system(x^4, 2x+2y^2,3x*y^2)
-
-    #     @test degrees(F) == [4,2,3]
-    #     @test nequations(F) == 3
-    #     @test nvars(F) == 2
-    # end
-
-    # @testset "weyl dot" begin
-    #     x,y = MPoly.generators(Complex128, :x,:y)
-
-    #     f = 3.0x^2 + 2.0x*y - 1.0*y^2
-    #     g = (-2.5+2im)x^2 + -3.0x*y + 4.0*y^2
-
-    #     F = MPoly.system(f)
-    #     G = MPoly.system(g)
-
-    #     @test weyl_dot(F,G) == 3.0 * conj(-2.5+2im) + 2.0 * (-3.0) / 2 + (-1.0) * 4.0
-    #     @test weyl_dot(F,F) == 9.0 + 4.0  / 2 + 1.0
-
-    #     @test weyl_dot(F,F) ≈ weyl_norm(F)^2
-    # end
 end
