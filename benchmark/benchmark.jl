1b3e7d22a3de44e8334ab28344e673d74b60dc5e
Sascha Timme <sascha.timme@gmail.com>
2017-08-14 14:34:53 +0200

    Add benchmark

---
+++ benchmark/benchmark.jl
@@ -0,0 +1,52 @@
+using HomotopyContinuation
+using BenchmarkTools
+
+function polysystem()
+    a = 5
+    b = 4
+    c = 3
+    sθ, cθ, z = MPoly.generators(Complex128, :sθ, :cθ, :z)
+    f1 = cθ^2 + sθ^2 - (1.0 + 0im)*z^2
+    f2 = (a*cθ - b)^2 + (1.0 + 0im) * (a*sθ)^2 - c^2 * z^2
+    F = MPoly.system([f1, f2])
+
+    x = rand(Complex128, 3)
+
+    @benchmark evaluate($F, $x)
+end
+
+polysystem()
+println("Run polysystem evaluation benchmark:")
+polysystem_benchmark = polysystem()
+show(STDOUT, MIME"text/plain"(), polysystem_benchmark)
+
+function triangle()
+    a = 5
+    b = 4
+    c = 3;
+
+    sθ, cθ = MPoly.generators(Complex128, :sθ, :cθ)
+    f1 = cθ^2 + sθ^2 - (1.0 + 0im)
+    f2 = (a*cθ - b)^2 + (1.0 + 0im) * (a*sθ)^2 - c^2
+    F = MPoly.system([f1, f2])
+
+    # lets use a total degree homotopy
+    G, start_solutions = total_degree(F)
+
+    # we will use a simple straight line homotopy
+    H = StraightLineHomotopy(G, F)
+
+    algorithm = PredictorCorrector.Spherical()
+    #now we can solve
+    results = solve(H, start_solutions, algorithm, report_progress=false)
+    results = map(x -> x.solution, filter(x -> x.retcode == :Success, results))
+    @assert (length(results) == 2)
+
+    @benchmark solve($H, $start_solutions, $algorithm, report_progress=false)
+end
+
+println("\n\nRun polysystem benchmark:")
+triangle_benchmark = triangle()
+show(STDOUT, MIME"text/plain"(), triangle_benchmark)
+
+println("\nDone.")
