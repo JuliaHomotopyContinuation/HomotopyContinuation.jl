3ca2403a7a282b7b02e1bd8bc7d3d66cc4db21ec
Sascha Timme <sascha.timme@gmail.com>
2017-08-21 14:21:25 +0200

    Include FixedHomotopy tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,7 @@ include("base_utilities_test.jl")
 
 include("straight_line_test.jl")
 include("gamma_trick_test.jl")
+include("fixedhomotopy_test.jl")
 include("spherical_test.jl")
 include("affine_test.jl")
 include("test_systems_test.jl")
