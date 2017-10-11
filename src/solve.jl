b7ceda77c4c6200ce2066934534c0f8523e01fb5
PB12345 <breiding@math.tu-berlin.de>
2017-10-11 08:36:23 +0200

    Now includes a direct solver for polynomial systems

--- src/solve.jl
+++ src/solve.jl
@@ -1,5 +1,8 @@
 export solve
 
+import MultivariatePolynomials
+const MP = MultivariatePolynomials
+
 """
     solve(H::AbstractHomotopy, startvalues_s, [algorithm]; kwargs...)
 
@@ -9,3 +12,21 @@ Solve the homotopy `H` via homotopy continuation with the given `startvalues_s` 
 function solve end
 
 solve(H::AbstractHomotopy, startvalue_s; kwargs...) = solve(H, startvalue_s, SphericalPredictorCorrector(); kwargs...)
+
+
+"""
+
+    solve(f::MP.AbstractPolynomial{T}; kwargs...)
+
+Solves the polynomial system f.
+As optional arguments you can declare the homotopytype (default = GeodesicOnTheSphere) and algorithm (default = SphericalPredictorCorrector()).
+"""
+function solve(f::MP.AbstractPolynomial{T}; homotopytype=GeodesicOnTheSphere, algorithm=SphericalPredictorCorrector()) where {T<:Number}
+      H,s = totaldegree(homotopytype,[f])
+      solve(H, s, algorithm)
+end
+
+function solve(f::Vector{<:MP.AbstractPolynomial{T}}; homotopytype=GeodesicOnTheSphere, algorithm=SphericalPredictorCorrector()) where {T<:Number}
+     H,s = totaldegree(homotopytype,f)
+     solve(H, s, algorithm)
+end
