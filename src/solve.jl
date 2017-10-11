4f47d575ae693bf3b01789c238cd8e5838267de0
Sascha Timme <sascha.timme@gmail.com>
2017-10-11 08:57:03 +0200

    Pass kwargs through in new solve methods

--- src/solve.jl
+++ src/solve.jl
@@ -1,32 +1,36 @@
 export solve
 
-import MultivariatePolynomials
-const MP = MultivariatePolynomials
-
 """
     solve(H::AbstractHomotopy, startvalues_s, [algorithm]; kwargs...)
 
 Solve the homotopy `H` via homotopy continuation with the given `startvalues_s` and the given
 `algorithm`.
+
+
+    solve(f::Vector{<:MP.AbstractPolynomial{T}}; homotopytype=GeodesicOnTheSphere, algorithm=SphericalPredictorCorrector(), kwargs...)
+    solve(f::MP.AbstractPolynomial{T}; homotopytype=GeodesicOnTheSphere, algorithm=SphericalPredictorCorrector(), kwargs...)
+
+Solves the polynomial system `f` via homotopy continuation. This uses a totaldegree homotopy
+of the given `homotopytype` and uses the given `algorithm` for pathtracking.
 """
 function solve end
 
-solve(H::AbstractHomotopy, startvalue_s; kwargs...) = solve(H, startvalue_s, SphericalPredictorCorrector(); kwargs...)
-
-
-"""
-
-    solve(f::MP.AbstractPolynomial{T}; kwargs...)
-
-Solves the polynomial system f.
-As optional arguments you can declare the homotopytype (default = GeodesicOnTheSphere) and algorithm (default = SphericalPredictorCorrector()).
-"""
-function solve(f::MP.AbstractPolynomial{T}; homotopytype=GeodesicOnTheSphere, algorithm=SphericalPredictorCorrector()) where {T<:Number}
-      H,s = totaldegree(homotopytype,[f])
-      solve(H, s, algorithm)
+function solve(H::AbstractHomotopy, startvalue_s; kwargs...)
+    solve(H, startvalue_s, SphericalPredictorCorrector(); kwargs...)
 end
 
-function solve(f::Vector{<:MP.AbstractPolynomial{T}}; homotopytype=GeodesicOnTheSphere, algorithm=SphericalPredictorCorrector()) where {T<:Number}
-     H,s = totaldegree(homotopytype,f)
-     solve(H, s, algorithm)
+function solve(
+    f::MP.AbstractPolynomial{T};
+    homotopytype=GeodesicOnTheSphere,
+    algorithm=SphericalPredictorCorrector(),
+    kwargs...) where {T<:Number}
+      H, s = totaldegree(homotopytype, [f])
+      solve(H, s, algorithm; kwargs...)
+end
+function solve(f::Vector{<:MP.AbstractPolynomial{T}};
+    homotopytype=GeodesicOnTheSphere,
+    algorithm=SphericalPredictorCorrector(),
+    kwargs...) where {T<:Number}
+     H, s = totaldegree(homotopytype, f)
+     solve(H, s, algorithm; kwargs...)
 end
