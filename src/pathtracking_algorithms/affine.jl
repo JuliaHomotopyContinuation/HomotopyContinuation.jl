d72e8fafc8684bf4e737c74dc95b7b8a99747d9b
Sascha Timme <sascha.timme@gmail.com>
2017-11-20 13:40:54 +0100

    Improve documentation for pathracking algorithms

--- src/pathtracking_algorithms/affine.jl
+++ src/pathtracking_algorithms/affine.jl
@@ -3,8 +3,17 @@ export AffinePredictorCorrector
 """
     AffinePredictorCorrector
 
-A prediction-correction based algorithm. As prediction it used single step of the explicit
-Euler method.
+This algorithm uses as an prediction step an explicit Euler method.
+Therefore the prediciton step looks like
+```math
+x_{k+1} = x_k - ΔtJ_H(x, t)^{-1}\\frac{∂H}{∂t}(x, t)
+```
+and the correction step looks like
+```math
+x_{k+1} = x_{k+1} - J_H(x, t)^{-1}H(x, t)
+```
+
+This algorithm tracks the path in the affine space.
 """
 struct AffinePredictorCorrector <: AbstractPathtrackingAlgorithm
 end
--- src/pathtracking_algorithms/spherical.jl
+++ src/pathtracking_algorithms/spherical.jl
@@ -3,10 +3,30 @@ export SphericalPredictorCorrector
 """
     SphericalPredictorCorrector
 
+This algorithm uses as an prediction step an explicit Euler method.
+For the prediction and correction step the Jacobian is augmented by Hermitian transposed
+``x^*`` to get a square system.
+Therefore the prediciton step looks like
+```math
+x_{k+1} = x_k - Δt\\begin{bmatrix}
+    J_H(x, t) \\\\
+    x^*
+\\end{bmatrix}^{-1}
+\\frac{∂H}{∂t}(x, t)
+```
+and the correction step looks like
+```math
+x_{k+1} = x_{k+1} - \\begin{bmatrix}
+    J_H(x, t) \\\\
+    x^*
+\\end{bmatrix}^{-1}
+H(x, t)
+```
 
-A prediction-correction based algorithm. As prediction it uses explicit euler.
-Jacobian is augmented by ``x{^H}`` to solve a square system during correction.
-**MORE DOCUMENTATION NEEDED**
+After each prediciton and correction the algorithm normalizes ``x`` again, i.e. ``x`` is
+then a point on the sphere again.
+
+This algorithm tracks the path in the projective space.
 """
 struct SphericalPredictorCorrector <: AbstractPathtrackingAlgorithm
 end
