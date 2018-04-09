0f8d72a31612fcbc4e569f831d3e8a8af8b7a9c7
Sascha Timme <sascha.timme@gmail.com>
2018-04-09 13:10:16 +0200

    Modify StepSize.update interface to also return status

--- src/dev/step_size.jl
+++ src/dev/step_size.jl
@@ -60,7 +60,9 @@ function initial_steplength end
 """
     update(curr_steplength, step::AbstractStepSize, state::AbstractStepSizeState, success::Bool)
 
-Returns the new step length. `success` indicates whether the step was successfull. This
+Returns `(t, status)` where `t` is the new step length and `status` is either
+`:ok` (usually) or  any symbol indicating why the update failed (e.g. `:steplength_too_small` if the step size is too small).
+The argument `success` indicates whether the step was successfull. This
 modifies `state` if necessary.
 """
 function update end
@@ -71,7 +73,8 @@ function update end
     HeuristicStepSize(;initial=0.1,
         increase_factor=2.0,
         decrease_factor=inv(increase_factor),
-        consecutive_successes_necessary=3)
+        consecutive_successes_necessary=3,
+        minimal_stepsize=eps())
 
 The step length is defined as follows. Initially the step length is `initial`.
 If `consecutive_successes_necessary` consecutive steps were sucessfull the step length
@@ -83,14 +86,16 @@ struct HeuristicStepSize <: AbstractStepSize
     increase_factor::Float64
     decrease_factor::Float64
     consecutive_successes_necessary::Int
+    minimal_stepsize::Float64
 end
 function HeuristicStepSize(;initial=0.1,
     increase_factor=2.0,
     decrease_factor=inv(increase_factor),
-    consecutive_successes_necessary=3)
+    consecutive_successes_necessary=3,
+    minimal_stepsize=eps())
 
     HeuristicStepSize(initial, increase_factor,
-        decrease_factor, consecutive_successes_necessary)
+        decrease_factor, consecutive_successes_necessary, minimal_stepsize)
 end
 
 mutable struct HeuristicStepSizeState <: AbstractStepSizeState
@@ -106,15 +111,21 @@ function update(curr_steplength, step::HeuristicStepSize, state::HeuristicStepSi
     if success
         if (state.consecutive_successes += 1) == step.consecutive_successes_necessary
             state.consecutive_successes = 0
-            return step.increase_factor * curr_steplength
+            return (step.increase_factor * curr_steplength, :ok)
         end
-        return curr_steplength
+        return (curr_steplength, :ok)
     end
     # reset successes
     state.consecutive_successes = 0
 
     # we decrease the steplength
-    step.decrease_factor * curr_steplength
+    new_steplength = step.decrease_factor * curr_steplength
+
+    if new_steplength < step.minimal_stepsize
+        return (new_steplength, :step_size_too_small)
+    end
+
+    (new_steplength, :ok)
 end
 
 end
