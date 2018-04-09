8e4ca499629a8d97f60beb3a7fb183d0a0b9c2b9
Sascha Timme <sascha.timme@gmail.com>
2018-04-09 10:58:46 +0200

    Disable old tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,16 +5,16 @@ import DynamicPolynomials
 const PolyImpl = DynamicPolynomials
 
 
-include("pathtracker_test.jl")
-include("solver_test.jl")
-include("affine_test.jl")
-include("spherical_test.jl")
-include("cauchyendgame_test.jl")
-include("pathcrossing_test.jl")
-#include("result_test.jl")
-include("test_systems_test.jl")
-include("solve_test.jl")
-# include("patch_test.jl")
+# include("pathtracker_test.jl")
+# include("solver_test.jl")
+# include("affine_test.jl")
+# include("spherical_test.jl")
+# include("cauchyendgame_test.jl")
+# include("pathcrossing_test.jl")
+# #include("result_test.jl")
+# include("test_systems_test.jl")
+# include("solve_test.jl")
+# # include("patch_test.jl")
 
 using TestSystems
 include("systems_test.jl")
