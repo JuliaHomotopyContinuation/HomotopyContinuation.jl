9df4a8c19ec4cdd38f8c8e1606eda174d4a418f1
Sascha Timme <sascha.timme@gmail.com>
2018-05-07 09:29:42 +0200

    Put HomotopyWithCache in separate file

--- src/homotopies.jl
+++ src/homotopies.jl
@@ -15,8 +15,7 @@ import ..Systems
 import ..Systems: AbstractSystem, AbstractSystemCache
 import ..Utilities: randomish_gamma
 
-export HomotopyWithCache,
-    AbstractHomotopy,
+export AbstractHomotopy,
     AbstractHomotopyCache,
     nvariables,
     cache,
@@ -27,27 +26,7 @@ export HomotopyWithCache,
     jacobian_and_dt!, jacobian_and_dt,
     precondition!, update!
 
-struct HomotopyWithCache{H<:AbstractHomotopy, C<:AbstractHomotopyCache} <: AbstractHomotopy
-    homotopy::H
-    cache::C
-end
-
-HomotopyWithCache(H::AbstractHomotopy, x, t) = HomotopyWithCache(H, cache(H, x, t))
-
-(H::HomotopyWithCache)(x, t) = evaluate(H, x, t)
-
-evaluate(H::HomotopyWithCache, x, t) = evaluate(H.homotopy, x, t, H.cache)
-evaluate!(u, H::HomotopyWithCache, x, t) = evaluate!(u, H.homotopy, x, t, H.cache)
-jacobian(H::HomotopyWithCache, x, t) = jacobian(H.homotopy, x, t, H.cache)
-jacobian!(U, H::HomotopyWithCache, x, t) = jacobian!(U, H.homotopy, x, t, H.cache)
-dt(H::HomotopyWithCache, x, t) = dt(H.homotopy, x, t, H.cache)
-dt!(u, H::HomotopyWithCache, x, t) = dt!(u, H.homotopy, x, t, H.cache)
-jacobian_and_dt(H::HomotopyWithCache, x, t) = jacobian_and_dt(H.homotopy, x, t, H.cache)
-jacobian_and_dt!(U, u, H::HomotopyWithCache, x, t) = jacobian_and_dt!(U, u, H.homotopy, x, t, H.cache)
-evaluate_and_jacobian(H::HomotopyWithCache, x, t) = evaluate_and_jacobian(H.homotopy, x, t, H.cache)
-evaluate_and_jacobian!(u, U, H::HomotopyWithCache, x, t) = evaluate_and_jacobian!(u, U, H.homotopy, x, t, H.cache)
-precondition!(H, x, t) = precondition!(H.homotopy, x, t, H.cache)
-update!(H, x, t) = update!(H.homotopy, x, t, H.cache)
+include("homotopies/homotopy_witch_cache.jl")
 
 include("homotopies/straight_line.jl")
 include("homotopies/fixed_point.jl")
