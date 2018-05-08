8bb68e6fcd1289f76c1207dee901e472ca0a77ea
Sascha Timme <sascha.timme@gmail.com>
2018-05-08 21:51:10 +0200

    Add unsafe_infinity_norm fallback again

--- src/utilities.jl
+++ src/utilities.jl
@@ -188,18 +188,18 @@ than `norm(z, Inf)`.
 Compute the ∞-norm of `z₁-z₂`.
 """
 infinity_norm(z::AbstractVector{<:Complex}) = sqrt(maximum(abs2, z))
-# function infinity_norm(z₁::AbstractVector{<:Complex}, z₂::AbstractVector{<:Complex})
-#     m = abs2(z₁[1] - z₂[1])
-#     n₁, n₂ = length(z₁), length(z₂)
-#     if n₁ ≠ n₂
-#         return convert(typeof(m), Inf)
-#     end
-#     @inbounds for k=2:n₁
-#         m = max(m, abs2(z₁[k] - z₂[k]))
-#     end
-#     sqrt(m)
-# end
-# unsafe_infinity_norm(v, w) = infinity_norm(v, w)
+function infinity_norm(z₁::AbstractVector{<:Complex}, z₂::AbstractVector{<:Complex})
+    m = abs2(z₁[1] - z₂[1])
+    n₁, n₂ = length(z₁), length(z₂)
+    if n₁ ≠ n₂
+        return convert(typeof(m), Inf)
+    end
+    @inbounds for k=2:n₁
+        m = max(m, abs2(z₁[k] - z₂[k]))
+    end
+    sqrt(m)
+end
+unsafe_infinity_norm(v, w) = infinity_norm(v, w)
 
 
 """
