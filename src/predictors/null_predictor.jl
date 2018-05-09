1edbd00e68b0b7feb1607322caf5222f123e13ca
Sascha Timme <sascha.timme@gmail.com>
2018-05-09 07:24:36 +0200

    Split predictors in separate files

---
+++ src/predictors/null_predictor.jl
@@ -0,0 +1,17 @@
+export NullPredictor, NullPredictorCache
+
+"""
+    NullPredictor()
+
+A predictor which does no prediction step, i.e., it just returns the input as
+its prediction.
+"""
+struct NullPredictor <: AbstractPredictor end
+struct NullPredictorCache <: AbstractPredictorCache end
+
+cache(::NullPredictor, H, x, t) = NullPredictorCache()
+
+function predict!(xnext, ::NullPredictor, ::NullPredictorCache, H, x, t, dt)
+    xnext .= x
+    nothing
+end
--- test/predictors_test.jl
+++ test/predictors_test.jl
@@ -1,14 +1,44 @@
-@testset "Predictors" begin
+function setup_prediction_test()
     F = Systems.SPSystem(equations(katsura6()))
     x = rand(Complex{Float64}, 7)
     xnext = copy(x)
     t = rand()
     H = Homotopies.HomotopyWithCache(Homotopies.StraightLineHomotopy(F, F), x, t)
+    H, x, xnext, t
+end
+
+@testset "Predictors.NullPredictor" begin
+    H, x, xnext, t = setup_prediction_test()
+
+    predictor = Predictors.NullPredictor()
+    @test predictor isa Predictors.NullPredictor
+    predictor_cache = Predictors.cache(predictor, H, x, t)
+    @test predictor_cache isa Predictors.NullPredictorCache
+
+    # check that this doesn't throw
+    Predictors.predict!(xnext, predictor, predictor_cache, H, x, t, 0.05)
+    @test xnext == x
+end
+
+@testset "Predictors.Euler" begin
+    H, x, xnext, t = setup_prediction_test()
 
     predictor = Predictors.Euler()
     @test predictor isa Predictors.Euler
     predictor_cache = Predictors.cache(predictor, H, x, t)
     @test predictor_cache isa Predictors.EulerCache
+
+    # check that this doesn't throw
+    Predictors.predict!(xnext, predictor, predictor_cache, H, x, t, 0.05)
+end
+
+@testset "Predictors.RK4" begin
+    H, x, xnext, t = setup_prediction_test()
+
+    predictor = Predictors.RK4()
+    @test predictor isa Predictors.RK4
+    predictor_cache = Predictors.cache(predictor, H, x, t)
+    @test predictor_cache isa Predictors.RK4Cache
 
     # check that this doesn't throw
     Predictors.predict!(xnext, predictor, predictor_cache, H, x, t, 0.05)
