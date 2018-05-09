69c0f9810f4bd4c55c3f110de4c3675a34ad755a
Sascha Timme <sascha.timme@gmail.com>
2018-05-09 08:51:20 +0200

    Split correctors into separate files

---
+++ src/correctors/newton.jl
@@ -0,0 +1,45 @@
+export Newton
+
+"""
+    Newton()
+
+A classical simple Newton operator for square linear systems using the LU factorization
+to solve the linear systems.
+"""
+struct Newton <: AbstractCorrector end
+
+struct NewtonCache{T} <: AbstractCorrectorCache
+    A::Matrix{T}
+    b::Vector{T}
+end
+
+function cache(::Newton, H::HomotopyWithCache, x, t)
+    A = jacobian(H, x, t)
+    b = evaluate(H, x, t)
+    NewtonCache(A, b)
+end
+
+function correct!(xnext, ::Newton, cache::NewtonCache, H::HomotopyWithCache, x, t, tol, maxiters)
+    A, b = cache.A, cache.b
+    evaluate_and_jacobian!(b, A, H, x, t)
+    solve_with_lu_inplace!(A, b)
+    @. xnext = x - b
+    k = 1
+    while true
+        evaluate!(b, H, xnext, t)
+        res = infinity_norm(b)
+        if res < tol
+            return Result(true, res, k)
+        elseif k ≥ maxiters
+            return Result(false, res, k)
+        end
+
+        k += 1
+
+        # put jacobian in A
+        jacobian!(A, H, xnext, t)
+
+        solve_with_lu_inplace!(A, b)
+        @. xnext = xnext - b
+    end
+end
