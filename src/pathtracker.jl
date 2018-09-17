b3faa4b88e1520347392ce69cd85ec6ffcc522e4
Sascha Timme <sascha.timme@gmail.com>
2018-09-17 15:44:05 +0200

    Add test for pathtracker_startsolutions

--- src/pathtracker.jl
+++ src/pathtracker.jl
@@ -10,8 +10,7 @@ to investigate single paths.
 function pathtracker_startsolutions(args...; kwargs...)
     supported, rest = splitkwargs(kwargs, Problems.supported_kwargs)
     prob, startsolutions = Problems.problem_startsolutions(args...; supported...)
-    t₁, t₀ = one(ComplexF64), zero(ComplexF64)
-    tracker = PathTracking.PathTracker(prob, startsolutions[1], t₁, t₀; rest...)
+    tracker = PathTracking.PathTracker(prob, first(startsolutions), one(ComplexF64), zero(ComplexF64); rest...)
 
     (tracker=tracker, startsolutions=startsolutions)
 end
