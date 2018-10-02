06f961f69682997333e392aea1ddda87399f0a52
Sascha Timme <sascha.timme@gmail.com>
2018-10-02 08:52:22 +0200

    Add pathtracker convenience function

--- src/pathtracker.jl
+++ src/pathtracker.jl
@@ -1,10 +1,10 @@
-export pathtracker_startsolutions
+export pathtracker_startsolutions, pathtracker
 
 """
     pathtracker_startsolutions(args...; kwargs...)
 
 Construct a [`PathTracking.PathTracker`](@ref) and `startsolutions` in the same way [`solve`](@ref)
-does it. This als0 takes the same input arguments as `solve`. This is convenient if you want
+does it. This also takes the same input arguments as `solve`. This is convenient if you want
 to investigate single paths.
 """
 function pathtracker_startsolutions(args...; kwargs...)
@@ -13,4 +13,16 @@ function pathtracker_startsolutions(args...; kwargs...)
     tracker = PathTracking.PathTracker(prob, Utilities.start_solution_sample(startsolutions), one(ComplexF64), zero(ComplexF64); rest...)
 
     (tracker=tracker, startsolutions=startsolutions)
+end
+
+"""
+    pathtracker(args...; kwargs...)
+
+Construct a [`PathTracking.PathTracker`](@ref) in the same way [`solve`](@ref)
+does it. This als0 takes the same input arguments as [`solve`](@ref). This is convenient if you want
+to investigate single paths.
+"""
+function pathtracker(args...; kwargs...)
+    tracker, _ = pathtracker_startsolutions(args...; kwargs...)
+    tracker
 end
