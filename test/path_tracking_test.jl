07b1e7086e563249de00fbe1fc4aea6669a7cb2e
Sascha Timme <sascha.timme@gmail.com>
2019-02-20 19:37:17 +0100

    Fix tests

--- test/path_tracking_test.jl
+++ test/path_tracking_test.jl
@@ -105,12 +105,12 @@
 
         tracker, S = pathtracker_startsolutions([x^2-y^2+4, x + y - 3])
         result = track(tracker, first(S), 1.0, 0.0)
-        @test result.returncode == :success
+        @test result.returncode == PathTrackerStatus.success
 
         # test type promotion of startsolutions
         tracker, S = pathtracker_startsolutions([x^2-y^2+4, x + y - 3])
         result = track(tracker, [1, 1], 1.0, 0.0)
-        @test result.returncode == :success
+        @test result.returncode == PathTrackerStatus.success
     end
 
     @testset "iterator" begin
