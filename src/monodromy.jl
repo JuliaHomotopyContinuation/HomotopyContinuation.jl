9386f56fe8a65e143a5e65c20ffe04d56e49ad7f
Sascha Timme <sascha.timme@gmail.com>
2019-03-10 09:29:56 +0100

    Move monodromy stuff all in one file

--- src/monodromy.jl
+++ src/monodromy.jl
@@ -1,49 +1,457 @@
 export monodromy_solve, realsolutions, nreal, GroupActions, complex_conjugation
 
-include("monodromy/group_actions.jl")
-include("monodromy/options.jl")
-include("monodromy/statistics.jl")
-include("monodromy/loop.jl")
+################
+# Group actions
+################
+"""
+    GroupActions(actions::Function...)
 
+Store a bunch of group actions `(f1, f2, f3, ...)`.
+Each action has to return a tuple.
+The actions are applied in the following sense
+1) f1 is applied on the original solution `s`
+2) f2 is applied on `s` and the results of 1
+3) f3 is applied on `s` and the results of 1) and 2)
+and so on
+
+## Example
+```julia-repl
+julia> f1(s) = (s * s,);
+
+julia> f2(s) = (2s, -s, 5s);
+
+julia> f3(s) = (s + 1,);
+
+julia> GroupActions(f1)(3)
+(9,)
+
+julia> GroupActions(f1,f2)(3)
+(9, 18, -9, 45)
+
+julia> GroupActions(f1,f2, f3)(3)
+(9, 18, -9, 45, 10, 19, -8, 46)
+```
+"""
+struct GroupActions{T<:Tuple}
+    actions::T
+end
+GroupActions(::Nothing) = GroupActions(())
+GroupActions(actions::GroupActions) = actions
+GroupActions(actions::Function...) = GroupActions(actions)
+
+function (G::GroupActions)(solution::V) where V
+    convert_if_necessary(apply_group_actions(G.actions, solution), solution)
+end
+convert_if_necessary(::Tuple{}, ::Any) = ()
+convert_if_necessary(xs::NTuple{N, V}, ::V) where {N, V} = xs
+convert_if_necessary(xs, ::V) where {V} = map(x -> convert(V, x), xs)
+
+apply_group_actions(::Tuple{}, solution) = ()
+# special case 1 function case
+apply_group_actions(fs::Tuple{<:Function}, solution) = (fs[1])(solution)
+# general case
+function apply_group_actions(actions::Tuple, solution)
+    f, rest = actions[1], Base.tail(actions)
+    foldl(rest; init=f(solution)) do acc, f
+        foldl(flatten, map(f, acc); init=acc)
+    end
+end
+flatten(a::Tuple, b::Tuple) = (a..., b...)
+
+#####################
+# Monodromy Options #
+#####################
+const monodromy_options_allowed_keywords = [:accuracy, :done_callback,
+    :group_action,:group_actions, :group_action_on_all_nodes,
+    :parameter_sampler, :target_solutions_count, :timeout,
+    :minimal_number_of_solutions, :maximal_number_of_iterations_without_progress]
+
+struct MonodromyOptions{F1<:Function, F2<:Tuple, F3<:Function}
+    accuracy::Float64
+    done_callback::F1
+    group_actions::GroupActions{F2}
+    group_action_on_all_nodes::Bool
+    parameter_sampler::F3
+    # stopping heuristic
+    target_solutions_count::Int
+    timeout::Float64
+    minimal_number_of_solutions::Int
+    maximal_number_of_iterations_without_progress::Int
+end
+
+function MonodromyOptions(isrealsystem;
+    accuracy::Float64=1e-6,
+    done_callback=always_false,
+    group_action=nothing,
+    group_actions=GroupActions(group_action),
+    group_action_on_all_nodes=false,
+    parameter_sampler=independent_normal,
+    # stopping heuristic
+    target_solutions_count=nothing,
+    timeout=float(typemax(Int)),
+    minimal_number_of_solutions::Int=default_minimal_number_of_solutions(target_solutions_count),
+    maximal_number_of_iterations_without_progress::Int=10)
+
+    if isrealsystem &&
+       (group_actions isa GroupActions{Tuple{}}) # i.e. no group_actions provided
+       actions = GroupActions(complex_conjugation)
+    else
+       actions = GroupActions(group_actions)
+   end
+
+    MonodromyOptions(accuracy, done_callback, actions,
+        group_action_on_all_nodes, parameter_sampler,
+        target_solutions_count == nothing ? typemax(Int) : target_solutions_count,
+        float(timeout),
+        minimal_number_of_solutions,
+        maximal_number_of_iterations_without_progress)
+end
+
+default_minimal_number_of_solutions(::Nothing) = 2
+function default_minimal_number_of_solutions(target_solutions_count::Int)
+    div(target_solutions_count, 2)
+end
+
+always_false(x, sols) = false
+
+"""
+    complex_conjugation(x)
+
+A group action which returns the elementwise complex conjugated solutions.
+"""
+complex_conjugation(x) = (conj.(x),)
+has_group_actions(options::MonodromyOptions) = !(options.group_actions isa GroupActions{Tuple{}})
+
+function independent_gaussian(p::SVector{N, T}) where {N, T}
+    p + (@SVector randn(T, N))
+end
+
+function independent_normal(p::SVector{N, T}) where {N, T}
+    @SVector randn(T, N)
+end
+
+##########################
+## Monodromy Statistics ##
+##########################
+
+mutable struct MonodromyStatistics
+    ntrackedpaths::Int
+    ntrackingfailures::Int
+    nreal::Int
+    nparametergenerations::Int
+    nsolutions_development::Vector{Int}
+end
+
+MonodromyStatistics(nsolutions::Int) = MonodromyStatistics(0, 0, 0, 1, [nsolutions])
+function MonodromyStatistics(solutions)
+    stats = MonodromyStatistics(length(solutions))
+    for s in solutions
+        checkreal!(stats, s)
+    end
+    stats
+end
+
+Base.show(io::IO, S::MonodromyStatistics) = print_fieldnames(io, S)
+Base.show(io::IO, ::MIME"application/prs.juno.inline", S::MonodromyStatistics) = S
+
+# update routines
+function trackedpath!(stats::MonodromyStatistics, retcode)
+    if retcode == PathTrackerStatus.success
+        stats.ntrackedpaths += 1
+    else
+        stats.ntrackingfailures += 1
+    end
+end
+
+function checkreal!(stats::MonodromyStatistics, y)
+    if isrealvector(y)
+        stats.nreal +=1
+    end
+end
+
+function generated_parameters!(stats::MonodromyStatistics, nsolutions::Int)
+    stats.nparametergenerations += 1
+    push!(stats.nsolutions_development, nsolutions)
+end
+
+function finished!(stats, nsolutions)
+    push!(stats.nsolutions_development, nsolutions)
+end
+
+
+#############################
+# Loops and Data Structures #
+#############################
+
+export Triangle, Petal
+
+#####################
+# Nodes of the Loop
+#####################
+
+"""
+    Node(p::SVector, x::AbstractVector; store_points=true, is_main_node=false)
+
+Create a node with a parameter from the same type as `p` and expecting
+points with tthe same type as `x`. If `stores_points` is `true` a `UniquePoints`
+data structure is allocated to keep track of all known solutions of this node.
+"""
+struct Node{N, T, UP<:UniquePoints}
+    p::SVector{N, T} # maybe just a Vector?
+    points::Union{Nothing, UP}
+    # Metadata / configuration
+    main_node::Bool
+end
+
+function Node(p::SVector{N, T}, x::AbstractVector; store_points=true, is_main_node=false) where {N, T}
+    uniquepoints = UniquePoints(typeof(x))
+    if store_points == false
+        Node{N, T, typeof(uniquepoints)}(p, nothing, is_main_node)
+    else
+        Node(p, uniquepoints, is_main_node)
+    end
+end
+function Node(p::SVector{N, T}, node::Node{N,T,UP}; store_points=true, is_main_node=false) where {N, T, UP}
+    if store_points == false
+        Node{N, T, UP}(p, nothing, is_main_node)
+    else
+        Node{N, T, UP}(p, UniquePoints(UP), is_main_node)
+    end
+end
+
+"""
+    add!(node::Node, x; kwargs...)
+
+Calls [`add!`](@ref) on the points of the Node.
+"""
+add!(node::Node, x; kwargs...) = add!(node.points, x; kwargs...)
+
+"""
+    iscontained(node::Node, x; kwargs...)
+
+Calls [`iscontained`](@ref) on the points of the Node.
+"""
+function iscontained(node::Node, x; kwargs...)
+    if node.points === nothing
+        false
+    else
+        iscontained(node.points, x; kwargs...)
+    end
+end
+
+"""
+    unsafe_add!(node::Node, x)
+
+Calls [`unsafe_add!`](@ref) on the points of the Node.
+"""
+function unsafe_add!(node::Node, x)
+    if node.points !== nothing
+        unsafe_add!(node.points, x)
+    end
+    nothing
+end
+
+
+#####################
+# Edges of the Loop
+#####################
+"""
+    Edge(start::Int, target::Int; usegamma=false)
+
+Create an edge between two nodes references by the index. `usegamma` refers
+to an optional weight of two random complex numbers γ=(γ₁, γ₀). These are the same
+gammas as in [`ParameterHomotopy`](@ref).
+
+    Edge(edge::Edge)
+
+Construct an indentical `Edge` as `edge`, but with newly samples γ (if applicable).
+"""
+struct Edge
+    start::Int
+    target::Int
+    γ::Union{Nothing, NTuple{2, ComplexF64}}
+end
+function Edge(start::Int, target::Int; usegamma=false)
+    if usegamma
+        γ = (randn(ComplexF64), randn(ComplexF64))
+    else
+        γ = nothing
+    end
+    Edge(start, target, γ)
+end
+function Edge(edge::Edge)
+    if edge.γ === nothing
+        edge
+    else
+        γ = (randn(ComplexF64), randn(ComplexF64))
+        Edge(edge.start, edge.target, γ)
+    end
+end
+
+#######################
+# Loop data structure
+#######################
+"""
+    Loop(p::SVector, x::AbstractVector{<:Number}, nnodes::Int, options::MonodromyOptions; usegamma=true)
+
+Construct a loop using `nnodes` nodes with parameters of the type of `p` and solutions
+of the type of `x`. `usegamma` refers to the use weights on the edges. See also
+[`Edge`](@ref).
+
+    Loop(p::SVector, x::AbstractVector, nnodes::Int, options::MonodromyOptions; usegamma=true)
+
+Construct the loop and add all points in `x` to it.
+
+    Loop(style::LoopStyle, p::SVector, x::AbstractVector, options::MonodromyOptions)
+
+Construct a loop using the defined style `style` with parameters of the type of `p` and solutions
+of the type of `x`.
+"""
+struct Loop{N<:Node}
+    nodes::Vector{N}
+    edges::Vector{Edge}
+end
+
+function Loop(p₁::SVector, x₁::AbstractVector{<:Number}, nnodes::Int, options::MonodromyOptions; usegamma=true)
+    n₁ = Node(p₁, x₁, is_main_node = true)
+    nodes = [n₁]
+    store_points = options.group_action_on_all_nodes && has_group_actions(options)
+    for i = 2:nnodes
+        p = options.parameter_sampler(p₁)
+        push!(nodes, Node(p, x₁, store_points=store_points))
+    end
+
+    loop = map(i -> Edge(i - 1, i; usegamma=usegamma), 2:nnodes)
+    push!(loop, Edge(nnodes, 1; usegamma=usegamma))
+
+    Loop(nodes, loop)
+end
+function Loop(p₁::SVector, x::AbstractVector, nnodes::Int, options::MonodromyOptions; kwargs...)
+    loop = Loop(p₁, first(x), nnodes, options; kwargs...)
+    for xᵢ ∈ x
+        add!(loop.nodes[1], xᵢ; tol=options.accuracy)
+    end
+    loop
+end
+
+"""
+    solutions(loop::Loop)
+
+Get the solutions of the loop.
+"""
+solutions(loop::Loop) = loop.nodes[1].points
+
+"""
+    nsolutions(loop::Loop)
+
+Get the number solutions of the loop.
+"""
+nsolutions(loop::Loop) = length(solutions(loop))
+
+"""
+    mainnode(loop::Loop)
+
+Get the main [`Node`](@ref) of the loop, i.e., the one with the original provided parameter.
+"""
+mainnode(loop::Loop) = loop.nodes[1]
+
+nextedge(loop::Loop, edge::Edge) = loop.edges[edge.target]
+
+##############
+# Loop styles
+##############
+"""
+    LoopStyle
+
+Abstract type defining a style of a loop.
+"""
+abstract type LoopStyle end
+
+
+"""
+    Triangle(;weights=true)
+
+A triangle is a loop consisting of the main node and two addtional nodes.
+If `weights` is true the edges are equipped with additional random weights.
+Note that this is usually only necessary for real parameters.
+"""
+struct Triangle <: LoopStyle
+    useweights::Bool
+end
+Triangle(;useweights=true) = Triangle(useweights)
+
+function Loop(strategy::Triangle, p::SVector, x::AbstractVector, options::MonodromyOptions)
+    Loop(p, x, 3, options, usegamma=strategy.useweights)
+end
+
+"""
+    Petal()
+
+A petal is a loop consisting of the main node and one other node connected
+by two edges with different random weights.
+"""
+struct Petal <: LoopStyle end
+function Loop(strategy::Petal, p::SVector, x::AbstractVector, options::MonodromyOptions)
+    Loop(p, x, 2, options, usegamma=true)
+end
+
+
+"""
+    regenerate!(loop::Loop, options::MonodromyOptions, stats::MonodromyStatistics)
+
+Regenerate all random parameters in the loop in order to introduce a new monodromy action.
+"""
+function regenerate!(loop::Loop, options::MonodromyOptions, stats::MonodromyStatistics)
+    main = mainnode(loop)
+
+    # The first node is the main node and doesn't get touched
+    for i ∈ 2:length(loop.nodes)
+        loop.nodes[i] = Node(options.parameter_sampler(main.p), loop.nodes[i])
+    end
+    loop.edges .= Edge.(loop.edges)
+    generated_parameters!(stats, length(main.points)) # bookkeeping
+end
+
+
+"""
+    track(tracker, x::AbstractVector, edge::Edge, loop::Loop, stats::MonodromyStatistics)
+
+Track `x` along the edge `edge` in the loop `loop` using `tracker`. Record statistics
+in `stats`.
+"""
+function track(tracker::PathTracker, x::AbstractVector, edge::Edge, loop::Loop, stats::MonodromyStatistics)
+    set_parameters!(tracker, edge, loop)
+    track(tracker, x, stats)
+end
+function track(tracker::PathTracker, x::AbstractVector, stats::MonodromyStatistics)
+    retcode = track!(tracker, x, 1.0, 0.0)
+    trackedpath!(stats, retcode)
+    retcode
+end
+
+"""x
+    set_parameters!(tracker::PathTracker, e::Edge, loop::Loop)
+
+Setup the parameters in the ParameterHomotopy in `tracker` to fit the edge `e`.
+"""
+function set_parameters!(tracker::PathTracker, e::Edge, loop::Loop)
+    H = basehomotopy(tracker.homotopy)
+    if !(H isa ParameterHomotopy)
+        error("Base homotopy is not a ParameterHomotopy")
+    end
+    p = (loop.nodes[e.start].p, loop.nodes[e.target].p)
+    set_parameters!(H, p, e.γ)
+end
+
+
+#############
+## Results ##
+#############
 struct MonodromyResult{N, T}
     returncode::Symbol
     solutions::Vector{SVector{N, T}}
     statistics::MonodromyStatistics
 end
-
-
-"""
-    mapresults(f::Function, result::MonodromyResult; conditions...)
-
-Apply the function `f` to all entries of `MonodromyResult` for which the given conditions apply.
-
-## Example
-```julia
-# This gives us all solutions considered real (but still as a complex vector).
-realsolutions = mapresults(solution, R, onlyreal=true)
-```
-"""
-function mapresults(f::Function, R::MonodromyResult;
-    onlyreal=false, realtol=1e-6)
-    [f(r) for r in R.solutions if
-        (!onlyreal || isrealvector(r, realtol))]
-end
-
-"""
-    solutions(result::MonodromyResult; conditions...)
-
-Return all solution (as `Vector`s) for which the given conditions apply.
-
-## Example
-```julia
-realsolutions = solutions(R, onlyreal=true)
-```
-"""
-function solutions(R::MonodromyResult; kwargs...)
-    mapresults(identity, R; kwargs...)
-end
-
-
 
 Base.iterate(R::MonodromyResult) = iterate(R.solutions)
 Base.iterate(R::MonodromyResult, state) = iterate(R.solutions, state)
@@ -89,11 +497,35 @@ end
 
 
 """
-    solutions(result::MonodromyResult)
+    mapresults(f, result::MonodromyResult; onlyreal=false, realtol=1e-6)
 
-Returns the solutions of the `result`.
+Apply the function `f` to all entries of `MonodromyResult` for which the given conditions apply.
+
+## Example
+```julia
+# This gives us all solutions considered real (but still as a complex vector).
+realsolutions = mapresults(solution, R, onlyreal=true)
+```
 """
-solutions(res::MonodromyResult) = res.solutions
+function mapresults(f, R::MonodromyResult;
+    onlyreal=false, realtol=1e-6)
+    [f(r) for r in R.solutions if
+        (!onlyreal || isrealvector(r, realtol))]
+end
+
+"""
+    solutions(result::MonodromyResult; onlyreal=false, realtol=1e-6)
+
+Return all solutions (as `SVector`s) for which the given conditions apply.
+
+## Example
+```julia
+realsolutions = solutions(R, onlyreal=true)
+```
+"""
+function solutions(R::MonodromyResult; kwargs...)
+    mapresults(identity, R; kwargs...)
+end
 
 """
     nsolutions(result::MonodromyResult)
@@ -103,23 +535,27 @@ Returns the number solutions of the `result`.
 nsolutions(res::MonodromyResult) = length(res.solutions)
 
 """
-    realsolutions(res::MonodromyResult; tol = 1e-6)
+    realsolutions(res::MonodromyResult; tol=1e-6)
 
-Returns the solutions of `res` whose imaginary part has norm < 1e-6.
+Returns the solutions of `res` whose imaginary part has norm less than 1e-6.
 """
 function realsolutions(res::MonodromyResult; tol=1e-6)
     map(r -> real.(r), filter(r -> LinearAlgebra.norm(imag.(r)) < tol, res.solutions))
 end
 
 """
-    nreal(res::MonodromyResult; tol = 1e-6)
+    nreal(res::MonodromyResult; tol=1e-6)
 
-Counts how many solutions of `res` have imaginary part norm < 1e-6.
+Counts how many solutions of `res` have imaginary part norm less than 1e-6.
 """
 function nreal(res::MonodromyResult; tol=1e-6)
     count(r -> LinearAlgebra.norm(imag.(r)) < tol, res.solutions)
 end
 
+
+#####################
+## monodromy solve ##
+#####################
 """
 MonodromyCache{FT<:FixedHomotopy, Tracker<:PathTracker, NC<:NewtonCache}
 
@@ -133,9 +569,6 @@ struct MonodromyCache{FT<:FixedHomotopy, Tracker<:PathTracker, NC<:NewtonCache, 
 end
 
 
-########
-# Setup
-########
 """
     monodromy_solve(F, sols, p; parameters=..., options..., pathtrackerkwargs...)
 
@@ -177,7 +610,7 @@ function monodromy_solve(F::Vector{<:MP.AbstractPolynomialLike{TC}},
 
     p₀ = convert(SVector{NParams, promote_type(Float64, TP)}, p₀)
 
-    optionskwargs, restkwargs = splitkwargs(kwargs, options_allowed_keywords)
+    optionskwargs, restkwargs = splitkwargs(kwargs, monodromy_options_allowed_keywords)
     options = begin
         isrealsystem = TC <: Real && TP <: Real
         MonodromyOptions(isrealsystem; optionskwargs...)
@@ -203,7 +636,6 @@ function monodromy_solve(F::Vector{<:MP.AbstractPolynomialLike{TC}},
 
     # construct cache
     C =  MonodromyCache(F₀, tracker, newton_cache, copy(tracker.state.x))
-
 
     # solve
     retcode = :not_assigned
@@ -238,9 +670,9 @@ function static_solutions(V::Vector{<:AbstractVector{<:Complex{<:AbstractFloat}}
 end
 
 
-##############
-# Actual work
-##############
+#################
+## Actual work ##
+#################
 
 """
     Job{N, T}
