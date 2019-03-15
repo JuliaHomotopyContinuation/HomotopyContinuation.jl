bd1c4b5834200b2fb78fc4504c0331e876396191
Sascha Timme <sascha.timme@gmail.com>
2019-03-15 16:18:50 +0100

    Also verify monodromy solutions during affine path tracking

--- src/monodromy.jl
+++ src/monodromy.jl
@@ -756,7 +756,13 @@ function empty_queue!(queue, loop::Loop, C::MonodromyCache, options::MonodromyOp
 end
 
 function verified_affine_vector(C::MonodromyCache, ŷ, x, options)
-    result = newton!(C.out, C.F, ŷ, euclidean_norm, C.newton_cache, options.identical_tol, 3, true)
+    # We distinguish solutions which have a distance larger than identical_tol
+    # But due to the numerical error in the evaluation of the distance, we need to be a little bit
+    # carfule. Therefore, we require that the solutions should be one magnitude closer to
+    # the true solutions as necessary
+    tol = 0.001 * options.identical_tol
+    result = newton!(C.out, C.F, ŷ, euclidean_norm, C.newton_cache, options.identical_tol, 5, false)
+
 
     if result.retcode == converged
         return affine_chart(x, C.out)
@@ -776,17 +782,9 @@ function process!(queue::Vector{<:Job}, job::Job, C::MonodromyCache, loop::Loop,
 
     node = loop.nodes[job.edge.target]
 
-    if node.main_node && (
-        !affine_tracking(C.tracker) ||
-        # Note, if auto scaling is enabled, and a solution component x_i is larg-ish
-        # then ||x_i - x^*_i|| is lower than the path tracking accuracy
-        # But if the path tracking accuracy is smaller than options.identical_tol
-        # The check would let the solution get through.
-        # TODO: Is that bad? Maybe if two solutions are very close?
-        (affine_tracking(C.tracker) && accuracy(C.tracker) > options.identical_tol)
-       )
+    if node.main_node
         y = verified_affine_vector(C, currx(C.tracker), job.x, options)
-        #is the solution at infinity?
+        # is the solution at infinity?
         if y === nothing
             return :incomplete
         end
