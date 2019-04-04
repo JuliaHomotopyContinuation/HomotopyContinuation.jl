383e0f82df4191d52acdc91da7ad97ba22281c49
Sascha Timme <sascha.timme@gmail.com>
2019-04-04 16:20:27 +0200

    Make it possible to create an affine and a projective TotalDegreeSystem

--- src/systems/totaldegree_system.jl
+++ src/systems/totaldegree_system.jl
@@ -8,33 +8,49 @@ Create a tottal degree system
 struct TotalDegreeSystem{T} <: AbstractSystem
     degrees::Vector{Int}
     degree_idxs::Vector{Int}
-    hom_idx::Int
+    hom_idx::Int # 0 if affine
     scaling_factors::Vector{T}
 
-    function TotalDegreeSystem(degrees::Vector{Int}, degree_idxs::Vector{Int}, hom_idx::Int,
-            scaling_factors::Vector{T}=ones(length(degrees))) where {T}
+    function TotalDegreeSystem(degrees::Vector{Int}, degree_idxs::Vector{Int}, hom_idx::Int=0,
+            scaling_factors::Vector{T}=ones(length(degrees)); affine=false) where {T}
         @assert length(degrees) == length(degree_idxs) == length(scaling_factors)
-        @assert 1 ≤ hom_idx ≤ length(degrees) + 1
+        if affine
+            hom_idx = 0
+        else
+            @assert 1 ≤ hom_idx ≤ length(degrees) + 1
+        end
         new{T}(degrees, degree_idxs, hom_idx, scaling_factors)
     end
 end
-function TotalDegreeSystem(F::Vector{<:MP.AbstractPolynomialLike}, args...)
-    TotalDegreeSystem(MP.maxdegree.(F), args...)
+function TotalDegreeSystem(F::Vector{<:MP.AbstractPolynomialLike}, args...; kwargs...)
+    TotalDegreeSystem(MP.maxdegree.(F), args...; kwargs...)
 end
-function TotalDegreeSystem(degrees::Vector{Int}, args...)
-    TotalDegreeSystem(degrees, collect(1:length(degrees)), length(degrees) + 1, args...)
+function TotalDegreeSystem(degrees::Vector{Int}, args...; kwargs...)
+    TotalDegreeSystem(degrees, collect(1:length(degrees)), length(degrees) + 1, args...; kwargs...)
 end
 
-Base.size(F::TotalDegreeSystem) = (length(F.degrees), length(F.degrees) + 1)
+function Base.size(F::TotalDegreeSystem)
+    n = length(F.degrees)
+    F.hom_idx == 0 ? (n, n) : (n, n + 1)
+end
 
 cache(::TotalDegreeSystem, x) = SystemNullCache()
 
-function evaluate!(u, F::TotalDegreeSystem, x, ::SystemNullCache)
+function evaluate!(u, F::TotalDegreeSystem, x::ProjectiveVectors.PVector, ::SystemNullCache)
+    λ = F.scaling_factors
+    @inbounds for i=1:length(F.degrees)
+        d = F.degrees[i]
+        u[i] = λ[i] * (x[i]^d - x[end]^d)
+    end
+    u
+end
+
+function evaluate!(u, F::TotalDegreeSystem, x::AbstractVector, ::SystemNullCache)
     λ = F.scaling_factors
     @inbounds for i=1:length(F.degrees)
         d = F.degrees[i]
         dix = F.degree_idxs[i]
-        u[i] = λ[i] * (x[dix]^d - x[F.hom_idx]^d)
+        u[i] = λ[i] * x[dix]^d - λ[i]
     end
     u
 end
@@ -44,46 +60,78 @@ function evaluate(F::TotalDegreeSystem, x, cache::SystemNullCache)
     u
 end
 
-function jacobian!(U, F::TotalDegreeSystem, x, ::SystemNullCache)
+function jacobian!(U, F::TotalDegreeSystem, x::PVector{<:Number, 1}, ::SystemNullCache)
     U .= zero(eltype(x))
-    hidx = F.hom_idx
+    λ = F.scaling_factors
+    N = length(x)
+    @inbounds for i=1:length(F.degrees)
+        d = F.degrees[i]
+        didx = F.degree_idxs[i]
+        if d == 1
+            U[i, didx] = λ[i]
+            U[i, N] = -λ[i]
+        elseif d > 1
+            U[i, didx] = λ[i] * d * x[didx]^(d-1)
+            U[i, N] = -λ[i] * d * x[N]^(d-1)
+        end
+    end
+    U
+end
+function jacobian!(U, F::TotalDegreeSystem, x::AbstractVector, ::SystemNullCache)
+    U .= zero(eltype(x))
     λ = F.scaling_factors
     @inbounds for i=1:length(F.degrees)
         d = F.degrees[i]
         didx = F.degree_idxs[i]
         if d == 1
             U[i, didx] = λ[i]
-            U[i, hidx] = -λ[i]
         elseif d > 1
             U[i, didx] = λ[i] * d * x[didx]^(d-1)
-            U[i, hidx] = -λ[i] * d * x[hidx]^(d-1)
         end
     end
     U
 end
+
 function jacobian(F::TotalDegreeSystem, x, cache::SystemNullCache)
     U = similar(x, size(F))
     jacobian!(U, F, x, cache)
     U
 end
 
-function evaluate_and_jacobian!(u, U, F::TotalDegreeSystem, x, ::SystemNullCache)
+function evaluate_and_jacobian!(u, U, F::TotalDegreeSystem, x::PVector{<:Number, 1}, ::SystemNullCache)
     U .= zero(eltype(x))
-    hidx = F.hom_idx
+    N = length(x)
     λ = F.scaling_factors
     @inbounds for i=1:length(F.degrees)
         d = F.degrees[i]
         didx = F.degree_idxs[i]
         if d == 1
-            u[i] = λ[i] * (x[didx] - x[hidx])
+            u[i] = λ[i] * (x[didx] - x[N])
             U[i, didx] = λ[i]
-            U[i, hidx] = -λ[i]
+            U[i, N] = -λ[i]
         elseif d > 1
             xd = x[didx]^(d-1)
-            xh = x[hidx]^(d-1)
-            u[i] = λ[i] * (xd * x[didx] - xh * x[hidx])
+            xh = x[N]^(d-1)
+            u[i] = λ[i] * (xd * x[didx] - xh * x[N])
             U[i, didx] = λ[i] * d * xd
-            U[i, hidx] = -λ[i] * d * xh
+            U[i, N] = -λ[i] * d * xh
+        end
+    end
+    nothing
+end
+function evaluate_and_jacobian!(u, U, F::TotalDegreeSystem, x::AbstractVector, ::SystemNullCache)
+    U .= zero(eltype(x))
+    λ = F.scaling_factors
+    @inbounds for i=1:length(F.degrees)
+        d = F.degrees[i]
+        didx = F.degree_idxs[i]
+        if d == 1
+            u[i] = λ[i] * x[didx] - λ[i]
+            U[i, didx] = λ[i]
+        elseif d > 1
+            xd = x[didx]^(d-1)
+            u[i] = λ[i] * xd * x[didx] - λ[i]
+            U[i, didx] = λ[i] * d * xd
         end
     end
     nothing
@@ -95,6 +143,3 @@ function evaluate_and_jacobian(F::TotalDegreeSystem, x, cache::SystemNullCache)
     evaluate_and_jacobian!(u, U, F, x, cache)
     u, U
 end
-
-
-weylnorm2(F::TotalDegreeSystem) = 2 * length(F.degrees)
