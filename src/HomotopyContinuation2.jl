163bc859b1ea6ddf8b33debd3d46869a18fa9e30
Sascha Timme <sascha@timme.xyz>
2020-02-03 11:03:52 +0100

    Reexport ModelKit stuff

--- src/HomotopyContinuation2.jl
+++ src/HomotopyContinuation2.jl
@@ -12,6 +12,35 @@ include("utils.jl")
 include("ModelKit.jl")
 import .ModelKit
 
+using .ModelKit: @var,
+                 @unique_var,
+                 Variable,
+                 Expression,
+                 variables,
+                 nvariables,
+                 subs,
+                 evaluate,
+                 differentiate,
+                 monomials,
+                 expand,
+                 System,
+                 Homotopy
+
+export @var,
+       @unique_var,
+       Variable,
+       Expression,
+       variables,
+       nvariables,
+       subs,
+       evaluate,
+       differentiate,
+       monomials,
+       expand,
+       System,
+       Homotopy
+
 include("linear_algebra.jl")
+include("homotopies.jl")
 
 end
