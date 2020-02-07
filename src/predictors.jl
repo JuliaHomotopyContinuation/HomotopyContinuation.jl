8d78655071e56b04158abda13121c7bf01a2d454
Sascha Timme <sascha@timme.xyz>
2020-02-07 13:55:30 +0100

    More robust pade predictor

--- src/predictors.jl
+++ src/predictors.jl
@@ -21,10 +21,9 @@ struct Pade21Cache <: AbstractPredictorCache
     dx²::NTuple{2,Vector{ComplexF64}}
     dx³::NTuple{3,Vector{ComplexF64}}
 
-    taylor::Base.RefValue{Bool}
-    order::Base.RefValue{Int}
-    trust_region::Base.RefValue{Float64}
-    err::Vector{ComplexF64}
+    taylor::BitVector
+    τ::Base.RefValue{Float64}
+    local_err::Vector{ComplexF64}
 end
 
 function cache(::Pade21, n::Int)
@@ -32,18 +31,24 @@ function cache(::Pade21, n::Int)
     x² = zeros(ComplexF64, n)
     x³ = zero(x²)
     x⁴ = zero(x²)
+
     u = zero(x²)
     dx¹ = (x¹,)
     dx² = (x¹,x²)
     dx³ = (x¹,x²,x³)
-    err = zero(u)
-    Pade21Cache(x¹, x², x³, x⁴, u, dx¹, dx², dx³, Ref(false), Ref(4), Ref(NaN), err)
+
+    taylor = falses(n)
+    τ = Ref(Inf)
+    local_err = zero(u)
+
+    Pade21Cache(x¹, x², x³, x⁴, u, dx¹, dx², dx³, taylor, τ, local_err)
 end
 
 function update!(cache::Pade21Cache, H, x, t, J::Jacobian)
     # unpack stuff to make the rest easier to read
     @unpack u, x¹, x², x³, x⁴, dx¹, dx², dx³ = cache
 
+    # comput all taylor coefficients x¹, x², x³, x⁴
     diff_t!(u, H, x, t)
     u .= .-u
     LA.ldiv!(x¹, J, u)
@@ -56,50 +61,51 @@ function update!(cache::Pade21Cache, H, x, t, J::Jacobian)
     u .= .-u
     LA.ldiv!(x³, J, u)
 
-    trust_region = Inf
-    for i in eachindex(x²)
-        trust_region = min(trust_region, abs(x²[i]) / abs(x³[i]))
-    end
+    diff_t!(u, H, x, t, dx³)
+    u .= .-u
+    LA.ldiv!(x⁴, J, u)
 
-    if isnan(trust_region)
-        cache.trust_region[] = Inf
-        cache.err .= x³
-        cache.taylor[] = true
-        cache.order[] = 3
-    else
-        cache.taylor[] = false
-        diff_t!(u, H, x, t, dx³)
-        u .= .-u
-        LA.ldiv!(x⁴, J, u)
+    # This is an adaption of the algorithm outlined in
 
-        cache.trust_region[] = trust_region
-        for i in eachindex(x²)
-            cache.err[i] = x⁴[i] - Base.FastMath.div_fast(x³[i]^2, x²[i])
+    τ = Inf
+    λ_min = exp2(-18)
+    for i in eachindex(x)
+        c¹ = fast_abs(x¹[i])
+        λ = min(λ_min, c¹)
+        c¹ /= λ
+        c² = fast_abs(x²[i]) / λ^2
+        c³ = fast_abs(x³[i]) / λ^3
+        tol = 1e-14 * max(c¹, c², c³)
+        if (c¹ ≤ tol && c² ≤ tol && c³ ≤ tol) || c² ≤ tol
+            cache.taylor[i] = true
+            cache.local_err[i] = x⁴[i]
+        else
+            τᵢ = (c² / c³) / λ
+            τᵢ < τ && (τ = τᵢ)
+            cache.local_err[i] = x⁴[i] - Base.FastMath.div_fast(x³[i]^2, x²[i])
         end
-        cache.order[] = 4
     end
+    cache.τ[] = τ
 
     cache
 end
 
 
 function predict!(x̂, cache::Pade21Cache, H, x, t, Δt)
-    @unpack x¹, x², x³ = cache
+    @unpack x¹, x², x³, taylor = cache
 
-    if cache.taylor[]
-        @inbounds for i in eachindex(x)
+    for i in eachindex(x)
+        if taylor[i]
             x̂[i] = x[i] + Δt * (x¹[i] + Δt * x²[i])
-        end
-    else
-        @inbounds for i in eachindex(x)
+        else
             δᵢ = 1 - Δt * x³[i] / x²[i]
-            x̂[i] = x[i] + Δt * x¹[i] + Base.FastMath.div_fast(Δt^2 * x²[i], δᵢ)
+            x̂[i] = x[i] + Δt * (x¹[i] + Δt * Base.FastMath.div_fast(x²[i], δᵢ))
         end
     end
 
     nothing
 end
 
-order(c::Pade21Cache) = c.order[]
-local_error(cache::Pade21Cache) = cache.err
-trust_region(cache::Pade21Cache) = cache.trust_region[]
+order(c::Pade21Cache) = 4
+local_error(cache::Pade21Cache) = cache.local_err
+trust_region(cache::Pade21Cache) = cache.τ[]
