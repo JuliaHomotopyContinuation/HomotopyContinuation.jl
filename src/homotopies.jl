5ab76d0c4edf53d8a9e8d6a977a0dd8c14e4dada
Sascha Timme <sascha@timme.xyz>
2020-02-26 09:51:25 +0100

    Add automatic scaling of the start system to make sure that coefficient sizes match

--- src/homotopies.jl
+++ src/homotopies.jl
@@ -36,7 +36,10 @@ function TotalDegreeStarts(degrees::Vector{Int})
     TotalDegreeStarts(degrees, iterator)
 end
 function Base.show(io::IO, iter::TotalDegreeStarts)
-    print(io, "$(length(iter)) total degree start solutions for degrees $(iter.degrees)")
+    print(
+        io,
+        "$(length(iter)) total degree start solutions for degrees $(iter.degrees)",
+    )
 end
 
 function Base.iterate(iter::TotalDegreeStarts)
@@ -72,7 +75,9 @@ function total_degree_homotopy(
     params::Vector{Variable} = Variable[];
     parameters::AbstractVector = ComplexF64[],
     gamma = cis(2π * rand()),
+    scaling = nothing,
 )
+    n = length(f)
     length(f) == length(vars) ||
     throw(ArgumentError("Given system does not have the same number of polynomials as variables."))
     length(params) == length(parameters) ||
@@ -81,10 +86,35 @@ function total_degree_homotopy(
     D = ModelKit.degree(f, vars)
 
     t = ModelKit.unique_variable(:t, vars, params)
-    γ = ModelKit.unique_variable(:γ, vars, params)
-    h = γ .* t .* (vars .^ D .- 1) .+ (1 .- t) .* f
 
-    H = ModelKitHomotopy(Homotopy(h, vars, t, [params; γ]), [parameters; gamma])
+    γ = ModelKit.unique_variable(:γ, vars, params)
+    all_params = [params; γ]
+    _s_ = Variable[]
+    for i = 1:n
+        _s_i = ModelKit.unique_variable(Symbol(:_s_, i), vars, all_params)
+        push!(_s_, _s_i)
+        push!(all_params, _s_i)
+    end
+    h = γ .* t .* _s_ .* (vars .^ D .- 1) .+ (1 .- t) .* f
+    if scaling == nothing
+        s = ones(n)
+        H = ModelKitHomotopy(
+            Homotopy(h, vars, t, all_params),
+            [parameters; gamma; s],
+        )
+        u = zeros(ComplexF64, n)
+        for i in 1:3
+            evaluate!(u, H, LA.normalize!(randn(ComplexF64, n)), 0.0)
+            s .= max.(s, exp2.(last.(frexp.(fast_abs.(u)))))
+        end
+        H.parameters[end-n+1:end] .= s
+    else
+        H = ModelKitHomotopy(
+            Homotopy(h, vars, t, all_params),
+            [parameters; gamma; scaling],
+        )
+    end
+
     S = TotalDegreeStarts(D)
     (homotopy = H, start_solutions = S)
 end
