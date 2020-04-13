0ce519d69485fa9bd4876a2dac891d36f85c0e2c
Sascha Timme <sascha@timme.xyz>
2020-04-13 09:18:34 +0200

    [Docs]: AffineChartHomotopy, AffineSubspaceHomotopy

--- src/homotopies/affine_subspace_homotopy.jl
+++ src/homotopies/affine_subspace_homotopy.jl
@@ -1,5 +1,16 @@
 export AffineSubspaceHomotopy
 
+
+"""
+    AffineSubspaceHomotopy(F::System, V::AffineSubspace, W::AffineSubspace)
+    AffineSubspaceHomotopy(F::AbstractSystem, V::AffineSubspace, W::AffineSubspace)
+
+Creates a homotopy ``H(x,t) = (F ∩ γ(t))(x)`` where ``γ(t)`` is a family of affine subspaces
+such that ``H(x,1) = (F ∩ V)(x)`` and ``H(x,0) = (F ∩ W)(x)``.
+Here ``γ(t)`` is the geodesic between `V` and `W` in the affine Grassmanian, i.e.,
+it is the curve of minimal length connecting `V` and `W`.
+See also [`AffineSubspace`](@ref) and [`geodesic`](@ref) and the references therein.
+"""
 struct AffineSubspaceHomotopy{S<:AbstractSystem} <: AbstractHomotopy
     system::S
 
