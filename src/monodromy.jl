177e18572203e2ecd3cbe7ddc338fb334e1f2c85
Sascha Timme <sascha@timme.xyz>
2020-11-30 16:35:03 +0100

    Fix formatting

--- src/monodromy.jl
+++ src/monodromy.jl
@@ -691,7 +691,7 @@ function find_start_pair_linear_in_params(F; kwargs...)
         if iszero(b)
             # don't want to have 0 as a parameter
             m == n && return nothing
-            p₀ = LA.nullspace(A)[:,1]
+            p₀ = LA.nullspace(A)[:, 1]
         else
             p₀ = LA.qr(A, Val(true)) \ b
         end
