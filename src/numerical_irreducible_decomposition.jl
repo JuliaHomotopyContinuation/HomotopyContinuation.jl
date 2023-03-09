ea5edf9d0b86c9301ed0fc6eb77fa6195207cfd6
PBrdng <pbrdng@gmail.com>
2023-03-09 11:19:28 +0100

    update io

--- src/numerical_irreducible_decomposition.jl
+++ src/numerical_irreducible_decomposition.jl
@@ -1194,16 +1194,18 @@ function degree_table(io, N::NumericalIrreducibleDecomposition)
     n = length(k)
 
     headers = ["dimension", "degrees of components"]
-    data = Matrix{Union{Int,Tuple}}(undef, n, 2)
-
+    data = Matrix{Union{Int, String}}(undef, n, 2)
 
     for (i, key) in enumerate(k)
         data[i, 1] = key
         components = Tuple(ModelKit.degree(W) for W in D[key])
         if length(components) == 1
             data[i, 2] = first(components)
-        else
-            data[i, 2] = components
+        elseif length(components) <= 10
+            data[i, 2] = string(components)
+        else 
+            s = string(components[1:10])
+            data[i, 2] = string("(", s[2:end-1], ", ...)")
         end
     end
 
