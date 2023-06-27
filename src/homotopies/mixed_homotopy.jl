2e12dd70354d2650e8d1f50ab508903f6c16cc60
Sascha Timme <me@saschatimme.com>
2023-06-27 20:18:15 +0200

    Fix method ambiguity

--- src/homotopies/mixed_homotopy.jl
+++ src/homotopies/mixed_homotopy.jl
@@ -28,5 +28,8 @@ ModelKit.evaluate!(u, H::MixedHomotopy, x::AbstractVector, t, p = nothing) =
     evaluate!(u, H.compiled, x, t, p)
 ModelKit.evaluate_and_jacobian!(u, U, H::MixedHomotopy, x, t, p = nothing) =
     evaluate_and_jacobian!(u, U, H.compiled, x, t, p)
-ModelKit.taylor!(u, v::Val, H::MixedHomotopy, tx, t, p = nothing) =
+ModelKit.taylor!(u, v::Val, H::MixedHomotopy, tx, t, p = nothing, ::Bool = false) =
     taylor!(u, v, H.interpreted, tx, t, p)
+
+ModelKit.taylor!(u, v::Val, H::MixedHomotopy, tx, t, ::Bool) =
+    taylor!(u, v, H.interpreted, tx, t, nothing)
