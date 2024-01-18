045a8eb72ffa3322c3f9e9618c83d80618523c13
PBrdng <pbrdng@gmail.com>
2024-01-18 10:24:04 +0100

    add one more test

--- test/certification_test.jl
+++ test/certification_test.jl
@@ -75,6 +75,20 @@
         @test ndistinct_real_certified(cert) == 4
     end
 
+    @testset "Reality Check" begin
+
+        @var x
+        x0 = [1.0]
+
+        F = System([x - 1 + 1e-16 * im])
+        certF = certify(F, [x0])
+        is_real(certificates(certF)[1]) == false
+
+        G = System([x - 1])
+        certG = certify(G, [x0])
+        is_real(certificates(certG)[1]) == true
+    end
+
     @testset "Parameters: Input = Vector{Expression}" begin
         @var x y
         f = (x^4 + y^4 - 1) * (x^2 + y^2 - 2) + x^5 * y
