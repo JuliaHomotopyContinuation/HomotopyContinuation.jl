86da331c92fa44782dfb4a412d27e22f35e28deb
Tim Holy <tim.holy@gmail.com>
2025-07-03 13:24:12 -0500

    Share `is_real` between HC and MK
    It turns out `ModelKit.is_real` was not the same function as
    `HomotopyContinuation.is_real`. This caused a conflict in the tests,
    where both `using HomotopyContinuation.ModelKit` and `using
    HomotopyContinuation` were used.
    
    This PR causes them to both define methods of the same `is_real`
    function.

--- src/ModelKit.jl
+++ src/ModelKit.jl
@@ -52,6 +52,7 @@ export @var,
     vectors
 
 using ..DoubleDouble: ComplexDF64
+import ..is_real    # required to ensure that ModelKit and HomotopyContinuation define methods of the same `is_real` function
 
 import Arblib: Arblib, Acb, AcbRef, AcbRefVector
 import SimpleGraphs
