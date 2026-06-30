d94c0c430f41e482938fcb873152522c97c47ad6
PBrdng <pbrdng@gmail.com>
2026-06-30 10:02:01 +0200

    fix doc typo

--- src/solve.jl
+++ src/solve.jl
@@ -265,7 +265,7 @@ end
 """
     linear_subspace_homotopy(F, V::LinearSubspace, W::LinearSubspace, intrinsic = nothing)
 
-Constructs an [`IntrinsicSubspaceHomotopy`](@ref) (if `dim(V) < codim(V)` or
+Constructs an [`IntrinsicSubspaceHomotopy`](@ref) (if `dim(V) <= codim(V)` or
 `intrinsic = true`) or [`ExtrinsicSubspaceHomotopy`](@ref).
 Compared to the direct constructor, this also takes care of homogeneous systems.
 """
